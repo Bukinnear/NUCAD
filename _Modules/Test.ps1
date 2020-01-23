ec00fe7b3311747a2920e7fec6cb7c13b7ffc2f3
Jared Kinnear <jared.kinnear@kiandra.com.au>
2020-01-23 12:45:54 +1100

    Reset test script contents

--- _Modules/Test.ps1
+++ _Modules/Test.ps1
@@ -4,13 +4,4 @@ $ErrorActionPreference = "Stop"
 # Path to new user creation module
 $Script:ScriptPath = (split-path -parent $MyInvocation.MyCommand.Definition)
 Import-Module -Name "$($ScriptPath)\NUC-AD" -Force
-
-$Identity = Get-ADUser "test.user1"
-$Groups = @("All Kiandra", "Melbourne Staff")
-
-Add-ADPrincipalGroupMembership -Identity $Identity -MemberOf "All Kiandra", "Melbourne Staff"
-
-<#
-"CN=Melbourne Staff,OU=Distribution Groups,OU=Kiandra,DC=kiandra,DC=local"
-"CN=All Kiandra,OU=Security Groups,OU=Kiandra,DC=kiandra,DC=local"
-#>
+#Import-Module -Name "PATH\NUC-AD" -Force
